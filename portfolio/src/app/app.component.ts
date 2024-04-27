ae633595b99196b3320b4866e80d5d4df6161db3
Gabiel Garcia Diaz <garturogdiaz@gmail.com>
2024-04-27 20:11:29 +0200

    translate ngx ok

--- portfolio/src/app/app.component.ts
+++ portfolio/src/app/app.component.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'portfolio';
+  constructor(private translate: TranslateService) {
+    translate.setDefaultLang('es');
+    translate.use('es');
+  }
+
+  useLanguage(language: string): void {
+    this.translate.use(language);
+  }
 }
