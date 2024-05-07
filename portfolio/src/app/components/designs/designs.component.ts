1bceccca9bf704f6090ce63d261f058b16e1e71e
Gabiel Garcia Diaz <garturogdiaz@gmail.com>
2024-05-07 14:08:41 +0200

    reflow content ok

--- portfolio/src/app/components/designs/designs.component.ts
+++ portfolio/src/app/components/designs/designs.component.ts
@@ -3,8 +3,5 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-designs',
   templateUrl: './designs.component.html',
-  styleUrls: ['./designs.component.scss']
 })
-export class DesignsComponent {
-
-}
+export class DesignsComponent {}
