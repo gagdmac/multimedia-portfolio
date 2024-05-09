b4b27baf1bbc26b1c4acefd82d15035e63e32a0a
Gabiel Garcia Diaz <garturogdiaz@gmail.com>
2024-05-09 10:45:09 +0200

    animejs ok

--- portfolio/src/app/components/home/home.component.ts
+++ portfolio/src/app/components/home/home.component.ts
@@ -1,7 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import anime from 'animejs/lib/anime.es.js';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
 })
-export class HomeComponent {}
+export class HomeComponent implements OnInit {
+  //call function Oninit
+  ngOnInit() {
+    this.animateSquare();
+  }
+
+  //Animejs function
+  animateSquare() {
+    const square = document.querySelector('.square');
+    anime({
+      targets: square,
+      translateX: '250px',
+
+      rotate: '1turn',
+      duration: 1000,
+      easing: 'easeInOutQuad',
+    });
+  }
+}
